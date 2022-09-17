f551067ae0eabb9737f082d6acfc33a5af046a1a
tahakhidr <tahakhidr@gmail.com>
2022-09-17 22:09:32 +0300

    implemented pre_save and post_save singnals to save slug

--- articles/models.py
+++ articles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+from django.db.models.signals import pre_save, post_save
 
 
 class Article(models.Model):
@@ -10,7 +11,23 @@ class Article(models.Model):
     last_updated = models.DateTimeField(auto_now=True)
     publish = models.DateField(null=True, blank=True)
 
-    def save(self, *args, **kwargs):
-        if self.slug is None:
-            self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     if self.slug is None:
+    #         self.slug = slugify(self.title)
+    #     super().save(args, kwargs)
+
+
+def article_pre_save(instance, *args, **kwargs):
+    if instance.slug is None:
+        instance.slug = slugify(instance.title)
+
+
+pre_save.connect(article_pre_save, sender=Article)
+
+
+def article_post_save(instance, created, *args, **kwargs):
+    if created:
+        print(f"{instance.title} was created")
+
+
+post_save.connect(article_post_save, sender=Article)
